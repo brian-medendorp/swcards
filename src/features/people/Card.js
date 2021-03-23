d6f93c420f120b058ee4426aec7c563fb1a34b32
Brian Medendorp <brian.medendorp@gmail.com>
2021-03-23 00:23:28 -0400

    additional styling

--- src/features/people/Card.js
+++ src/features/people/Card.js
@@ -5,10 +5,10 @@ export function Card(props) {
   return (
     <article className={styles.card}>
       <h1>{props.person.name}</h1>
-      <div><label>Origin:</label> {props.person.homeworld}</div>
-      <div><label>Height:</label> {props.person.height}</div>
-      <div><label>Mass:</label> {props.person.mass}</div>
-      <div><label>Birth date:</label> {props.person.birth_year}</div>
+      <div className={styles.attr}><label>Origin:</label> {props.person.homeworld}</div>
+      <div className={styles.attr}><label>Height:</label> {props.person.height}</div>
+      <div className={styles.attr}><label>Mass:</label> {props.person.mass}</div>
+      <div className={styles.attr}><label>Birth date:</label> {props.person.birth_year}</div>
     </article>
   )
 }
