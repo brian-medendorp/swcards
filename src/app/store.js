6d2d8b55351a72b537a70092b46408095c8cb0f9
Brian Medendorp <brian.medendorp@gmail.com>
2021-03-28 14:55:27 -0400

    remove example counter from original template

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import peopleReducer from '../features/people/peopleSlice';
 import planetReducer from '../features/planet/planetSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     people: peopleReducer,
     planet: planetReducer
   },
